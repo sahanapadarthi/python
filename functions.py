d17caace8c99d6d7211f211ebdcffefcf71580d1
sahanapadarthi <123532402+sahanapadarthi@users.noreply.github.com>
2023-02-12 13:44:59 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,7 @@
+#number functions
+import math
+print(int(math.pow(3,3)))
+a=math.floor(4.5)
+b=math.floor(11)
+print(a)
+print(b)
---
+++ loops.py
@@ -0,0 +1,9 @@
+#for loop
+#for num in range(0,10,2):
+ #   print(num)
+
+#while loop
+num=0
+while(num<10):
+    print (num)
+    num=num+1 
